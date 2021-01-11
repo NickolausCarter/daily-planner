fd87d7abd63977d6a96561fc7cac3f551c865329
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-01-10 23:31:58 -0600

    simplified loadTasks to make functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,24 +9,38 @@ var loadTasks = function() {
 
   if (!tasks) {
     tasks = {};
+  } else {
+    // need to work on iterating through objects
+    $("#task9").val(tasks[0])
+    $("#task10").val(tasks[1])
+    $("#task11").val(tasks[2])
+    $("#task12").val(tasks[3])
+    $("#task1").val(tasks[4])
+    $("#task2").val(tasks[5])
+    $("#task3").val(tasks[6])
+    $("#task4").val(tasks[7])
+    $("#task5").val(tasks[8])
   }
-
-    var index = $(".row").index();
-    Object.entries(tasks).forEach();
-    console.log(taskArr)
 };
 
+// provide warning to user that changes are not saved to localStorage
+$(".row").on("keyup", "textarea", function() {
+  $(this).closest(".row").find(".fa-save").addClass("fa-spin");
+})
+
+// simple save function
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-
-
+// save edits to tasks object at index to make iteration easier (I think)
 $(".saveBtn").click(function() {
   var taskEdit = $(this).closest(".row").find(".taskText").val();
   var index = $(this).closest(".row").index();
   tasks[index] = taskEdit;
   saveTasks();
+  // stop save icon spin to indicate localStorage is up-to-date
+  $(this).find(".fa-save").removeClass("fa-spin");
 });
 
 //     $(tasks).forEach(function(task) {
